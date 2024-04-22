733fedb545df2d985189e001d34e8bc25c897f46
flaitenberger <f.laitenberger@gmail.com>
2024-04-22 14:07:22 +0200

    test

---
+++ experiment/tests/test_sample.py
@@ -0,0 +1,5 @@
+def test_addition():
+    assert 1 + 2 == 3
+
+def test_assert_false():
+    assert False
--- jobs/environment.sh
+++ jobs/environment.sh
@@ -4,4 +4,4 @@ module load Anaconda3/2022.05
 
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
-conda activate cephalometry
+conda activate fomo
