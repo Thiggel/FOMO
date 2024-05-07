a6129c9f94a38ac0d007e69e23e03d042ec11cb4
Stefan Vasilev <ste5vas@gmail.com>
2024-05-07 11:24:24 +0200

    tensors to numpy in test_ood

--- tests/test_ood.py
+++ tests/test_ood.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 from experiment.ood.ood import extract_features, ood
+import numpy as np
 
 class DummyDataset(Dataset):
     def __init__(self, data):
@@ -30,13 +31,13 @@ def test_extract_features():
     train_features, val_features = extract_features(train_dataset, val_dataset, feature_extractor)
     
     # Assert the expected output
-    assert torch.all(torch.eq(train_features, torch.tensor([[1, 2, 3], [4, 5, 6]])))
-    assert torch.all(torch.eq(val_features, torch.tensor([[7, 8, 9], [10, 11, 12]])))
+    assert np.all(np.equal(train_features.numpy(), np.array([[1, 2, 3], [4, 5, 6]])))
+    assert np.all(np.equal(val_features.numpy(), np.array([[7, 8, 9], [10, 11, 12]])))
     
 def test_ood():
     # Create dummy features
-    train_features = torch.tensor([[0, 0, 1], [0, 1, 0]])
-    val_features = torch.tensor([[0, 0, 0], [10, 5, 8]])
+    train_features = np.array([[0, 0, 1], [0, 1, 0]])
+    val_features = np.array([[0, 0, 0], [10, 5, 8]])
     
     # Call the ood function
     ood_scores, thresh = ood(train_features, val_features, 0.5)
