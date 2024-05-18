37217e0a6a4b3ac0437c21e83c982a0fe5d62e82
Stefan Vasilev <ste5vas@gmail.com>
2024-05-18 13:00:30 +0200

    update_dataset path fix

--- experiment/ImbalancedTraining.py
+++ experiment/ImbalancedTraining.py
@@ -76,7 +76,7 @@ class ImbalancedTraining:
         self.generate_new_data(ood_samples, pipe=diffussion_pipe, save_subfolder=f"{self.args.additional_data_path}/{cycle_idx}")
 
         self.datamodule.update_dataset(
-            path=f"{self.args.additional_data_path}/{cycle_idx}"
+            aug_path=f"{self.args.additional_data_path}/{cycle_idx}"
         )
 
     def pretrain_imbalanced(
