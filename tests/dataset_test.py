57db218ca8966309c3bf8a50f0dac6a5c1978b1b
Stefan Vasilev <ste5vas@gmail.com>
2024-05-18 22:13:15 +0200

    transform deterministic to tensor in ds test

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -4,11 +4,13 @@ from torchvision.utils import save_image
 from experiment.dataset.ImageNetVariants import ImageNetVariants
 from experiment.dataset.imbalancedness.ImbalanceMethods import ImbalanceMethods
 import os
+from torchvision import transforms
 
 def test_update_dataset():
     # Create an instance of ImbalancedImageNetDataModule
     datamodule = ImbalancedImageNetDataModule(dataset_variant=ImageNetVariants.ImageNetDummy,
-                                              imbalance_method=ImbalanceMethods.NoImbalance)  
+                                              imbalance_method=ImbalanceMethods.NoImbalance,
+                                              transform=transforms.ToTensor())  
     
     initial_length = len(datamodule.train_dataset)
 
@@ -30,10 +32,7 @@ def test_update_dataset():
 
     # Check that the dataset has been updated
     assert torch.equal(datamodule.train_dataset[initial_length][0], torch.zeros(datamodule.train_dataset[0][0].shape))
-    assert datamodule.train_dataset[initial_length][1] == 0
-
-    assert torch.equal(datamodule.train_dataset[initial_length + 1][0], torch.ones(datamodule.train_dataset[0][0].shape))
-    assert datamodule.train_dataset[initial_length + 1][1] == 1
+    assert torch.equal(datamodule.train_dataset[initial_length+1][0], torch.zeros(datamodule.train_dataset[0][0].shape))
 
     # Remove the dummy image file
     os.remove(image_filename)
