1a2b45cbce2cf453f1fa28f0bf8bef5e5b51d262
Filipe Laitenberger <filipe@eduvpn-uva-stu-145-3-1-229.vpn.uva.nl>
2024-10-05 13:37:36 +0200

    resolved merge conflicts

--- jobs/ViTBase/ablations/stepwiseBaseline.sh
+++ jobs/ViTBase/ablations/stepwiseBaseline.sh
@@ -1,11 +1,7 @@
 . jobs/environment.sh
 
 python -m experiment \
-<<<<<<< HEAD:jobs/ViTBase/ablations/stepwiseBaseline.sh
 	--model ViTBase \
-=======
-	--model ResNet50 \
->>>>>>> f6dbd7cfbc211eea89b4c0701e0db2c335087910:jobs/ablations/ResNet50/stepwiseBaseline.sh
 	--imbalance_method power_law_imbalance \
 	--max_cycles 5 \
 	--ood_augmentation \
