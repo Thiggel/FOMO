7664c2e4d6a991de5d59c6a2fb948bd2a89a6f6f
Filipe Laitenberger <filipe@mac.localdomain>
2024-10-29 08:14:03 +0100

    imagenet 1k simclr job files

---
+++ jobs/ImageNet1k/ResNet50_SimCLR_Imbalanced.sh
@@ -0,0 +1,10 @@
+. jobs/environment.sh
+
+python -m experiment \
+	--model_name ResNet50 \
+  --num_runs 1 \
+  --imagenet_variant 1k \
+	--max_cycles 1 \
+	--n_epochs_per_cycle 100 \
+  --crop_size 96 \
+  --experiment_name "Baseline_ResNet50_SimCLR_Imbalanced_ImageNet1k"
