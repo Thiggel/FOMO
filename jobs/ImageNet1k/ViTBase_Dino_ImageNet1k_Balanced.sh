11eefc300786e9be71ca856af3b23a77c0718f65
Thiggel <f.laitenberger@gmail.com>
2024-11-20 17:23:55 +0100

    .

--- jobs/ImageNet1k/ViTBase_Dino_ImageNet1k_Balanced.sh
+++ jobs/ImageNet1k/ViTBase_Dino_ImageNet1k_Balanced.sh
@@ -8,8 +8,8 @@ python -m experiment \
   --num_runs 1 \
   --imagenet_variant 1k \
 	--n_epochs_per_cycle 300 \
-  --batch_size 64 \
-  --grad_acc_steps 16 \
+  --batch_size 56 \
+  --grad_acc_steps 18 \
   --crop_size 224 \
   --experiment_name "Baseline_ViTBase_Dino_ImageNet1k_Balanced"
 
