02a363b6f71162b3fbe3be6f11bc5a7784b26bbe
Thiggel <f.laitenberger@gmail.com>
2024-11-20 17:00:35 +0100

    upload ceckpoints to wandb

--- experiment/__main__.py
+++ experiment/__main__.py
@@ -158,7 +158,10 @@ def run(
     if args.logger and not args.test_mode:
         log_name = args.experiment_name if args.experiment_name else checkpoint_filename
         wandb_logger = WandbLogger(
-            project="FOMO2", name=log_name + str(seed), group=log_name
+            project="FOMO2",
+            name=log_name + str(seed),
+            group=log_name,
+            log_model="all",
         )
         wandb_logger.watch(model, log="all")
 
