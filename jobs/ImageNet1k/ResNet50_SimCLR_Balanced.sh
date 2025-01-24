cfc98a3366783382064bb88c08eb9fa2db444675
Thiggel <f.laitenberger@gmail.com>
2025-01-24 15:49:37 +0100

    simclr resnet50 imagenet1k job

--- jobs/ImageNet1k/ResNet50_SimCLR_Balanced.sh
+++ jobs/ImageNet1k/ResNet50_SimCLR_Balanced.sh
@@ -2,11 +2,11 @@
 
 python -m experiment \
 	--model_name ResNet50 \
-  --num_runs 1 \
+  --num_runs 3 \
   --imagenet_variant 1k \
 	--imbalance_method no_imbalance \
 	--max_cycles 1 \
 	--n_epochs_per_cycle 100 \
-  --crop_size 96 \
+  --batch_size 4096 \
   --experiment_name "Baseline_ResNet50_SimCLR_Balanced_ImageNet1k"
 
