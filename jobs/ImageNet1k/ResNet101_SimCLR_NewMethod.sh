8698b6481a64fd40a13d1d352166f8423409f471
Filipe Laitenberger <f.laitenberger@gmail.com>
2025-07-14 17:31:08 +0200

    Add temperature schedule flags to TS job scripts

--- jobs/ImageNet1k/ResNet101_SimCLR_NewMethod.sh
+++ jobs/ImageNet1k/ResNet101_SimCLR_NewMethod.sh
@@ -6,8 +6,12 @@ python -m experiment \
   --imagenet_variant 1k \
 	--max_cycles 5 \
 	--ood_augmentation \
-	--n_epochs_per_cycle 20 \
-	--pct_ood 0.15 \
+        --n_epochs_per_cycle 20 \
+        --pct_ood 0.15 \
   --crop_size 96 \
   --batch_size 4096 \
+        --use-temperature-schedule \
+        --temperature-min 0.1 \
+        --temperature-max 1.0 \
+        --t-max 400 \
   --experiment_name "Baseline_ResNet101_SimCLR_NewMethod_ImageNet1k"
--- jobs/ImageNet1k/ResNet50_SimCLR_AllData_NewMethod.sh
+++ jobs/ImageNet1k/ResNet50_SimCLR_AllData_NewMethod.sh
@@ -8,6 +8,10 @@ python -m experiment \
 	--max_cycles 5 \
 	--ood_augmentation \
 	--n_epochs_per_cycle 20 \
-	--pct_ood 0.075 \
-	--crop_size 96 \
+        --pct_ood 0.075 \
+        --crop_size 96 \
+        --use-temperature-schedule \
+        --temperature-min 0.1 \
+        --temperature-max 1.0 \
+        --t-max 400 \
   --experiment_name "NewMethod_ResNet50_SimCLR_Imbalanced_ImagNet1k_AllData"
