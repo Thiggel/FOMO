5edb080ca62fcafed063cb0698b5525038a0599d
SMSD75 <s.salehidehnavi@uva.nl>
2025-08-14 11:24:38 +0200

    environment

---
+++ jobs/environment.sh
@@ -0,0 +1,37 @@
+module purge
+module load gcc/default
+module load cuda12.1/toolkit/12.1
+
+export SCRATCH_LOCAL="/var/scratch/ssalehid"
+
+cd $SCRATCH_LOCAL
+mkdir FOMO
+
+cd $HOME/FOMO
+
+# Base directory
+export BASE_CACHE_DIR="$SCRATCH_LOCAL/FOMO"
+
+# Hugging Face
+export HF_HOME="$BASE_CACHE_DIR"
+export HF_DATASETS_CACHE="$BASE_CACHE_DIR/datasets"
+export TRANSFORMERS_CACHE="$BASE_CACHE_DIR/transformers"
+export HF_MODULES_CACHE="$BASE_CACHE_DIR/modules"
+export TMPDIR="$BASE_CACHE_DIR/tmp"
+
+# DeepSpeed
+export DEEPSPEED_CACHE_DIR="$BASE_CACHE_DIR/deepspeed"
+
+# Weights & Biases
+export WANDB_DIR="$BASE_CACHE_DIR"
+
+# PyTorch Lightning
+# Note: PyTorch Lightning doesn't use an environment variable, 
+# but you can use this in your Python code
+export PYTORCH_LIGHTNING_HOME="$BASE_CACHE_DIR/lightning_logs"
+
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
+
+activate fomo
+
+pip install -r requirements.txt
