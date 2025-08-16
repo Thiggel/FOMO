a33e8a82deb288ac20c5bc6a7a039b91419603a3
Filipe Laitenberger <f.laitenberger@gmail.com>
2025-08-16 11:48:14 +0200

    Organize SOTA jobs by dataset

--- jobs/sota/newmethod_ts.sh
+++
@@ -1,6 +0,0 @@
-cd $HOME/FOMO
-
-. jobs/environment.sh
-
-python -m experiment model=resnet50 ssl=simclr dataset=imagenet1k_imbalanced max_cycles=1 n_epochs_per_cycle=800 use_temperature_schedule=true ood_augmentation=true experiment_name=sota_newmethod_ts
-
